2ab6e78af7a05892b4b24fa7bd4ecd4957f3b0a2
chinsun9 <chin_sung@naver.com>
2021-09-28 12:41:28 +0900

    feat: count

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const targetElement = document.querySelector('.target');
 const toggleButtonElement = document.querySelector('.toggleBtn');
+const countElement = document.querySelector('.count');
 
 toggleButtonElement.addEventListener('click', () => {
   targetElement.classList.toggle('hello');
@@ -8,7 +9,11 @@ toggleButtonElement.addEventListener('click', () => {
 const observer = new MutationObserver((mutationsList) => {
   mutationsList.forEach((item) => {
     console.log(item);
-    if (item.target.classList.contains('hello')) console.log('hello!');
+    if (item.target.classList.contains('hello')) {
+      console.log('hello!');
+      const count = parseInt(countElement.innerHTML);
+      countElement.innerHTML = count + 1;
+    }
   });
 });
 
